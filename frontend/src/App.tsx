92215a011b132e76165ce4b3aaf5074b1f8a85b8
Agx <theankushgautam@gmail.com>
2026-09-13 16:10:09 +0545

    feat(api): connected backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
-function App() {
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [msg, setMsg] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/health")
+      .then((response) => response.json())
+      .then((response) => setMsg(response.message))
+      .catch((e) => console.error(e));
+  }, []);
 
   return (
     <>
       <h1>Birdie</h1>
+
+      <p>Health check : {msg}</p>
     </>
   )
 }
